f1446d6542a9aee783304882ccb9bef54d893ac8
Clouser <clouser24ryan@gmail.com>
2025-03-10 13:54:48 -0400

    added typedefs

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import {typeDefs} from './schema.js'
 
+//server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+})
+
+console.log("server ready at port", 4000)
